91c958a2ad63e743c430f11f7e850d4671ac987e
TheOmegaBlack <sachamorgese@gmail.com>
2017-04-05 01:07:33 +0900

    Turn 1 complete! Moving on!

--- Simon/Simon.js
+++ Simon/Simon.js
@@ -14,7 +14,9 @@ $(document).ready(function () {
     var isPlayerTurn = false;
     var onAnim = false;
     var buttons = [greenButton, redButton, yellowButton, blueButton];
-    var gameArray = [];
+    var inactiveColors = ["green", "red", "yellow", "blue"];
+    var activeColors = ["greenact", "redact", "yellowact", "blueact"];
+    var gameArray;
     var starsCount;
     var scrollingString = "*******Simon freecodecamp by Sacha Morgese*******"
     var greenSound = new Audio("Audio/simonSound1.mp3");
@@ -22,6 +24,7 @@ $(document).ready(function () {
     var yellowSound = new Audio("Audio/simonSound3.mp3");
     var blueSound = new Audio("Audio/simonSound4.mp3");
     var sounds = [greenSound, redSound, yellowSound, blueSound];
+    var stringInterval;
 
     function animationStarsOff() {
         displayText.text("")
@@ -32,6 +35,8 @@ $(document).ready(function () {
         else{
             if(onAnim){
                 onAnim = false;
+                startButton.prop("disabled", false);
+                strictButton.prop("disabled", false);  
                 scrolling();   
             }
         }
@@ -50,6 +55,7 @@ $(document).ready(function () {
                 displayText.text("");
             }, 500);
             clearInterval(interval);
+            onButton.prop("disabled", false);
         }
     }
     
@@ -67,8 +73,8 @@ $(document).ready(function () {
         else {
             size = 9;
         }
-        var interval = setInterval(function(){
-            scroll(index, interval, size);
+        stringInterval = setInterval(function(){
+            scroll(index, stringInterval, size);
             index++;
         }, 100);
     }
@@ -76,32 +82,68 @@ $(document).ready(function () {
     function onAnimation() {
         onAnim = true;
         starsCount = 0;
-        setTimeout(animationStars, 200);
-        startButton.prop("disabled", false);
-        strictButton.prop("disabled", false);
-        colorButton.prop("disabled", false);
-       
+        setTimeout(animationStars, 200);     
     }
 
     function offAnimation() {
+        displayText.text("Goodbye");
+        setTimeout(function(){displayText.text("")}, 1000)
         startButton.prop("disabled", true);
         strictButton.prop("disabled", true);
+        colorButton.prop("disabled", true);
     }
 
     function makeArray() {
         gameArray = [];
         for (var i = 0; i < 20; i++) {
             var rndm = Math.floor(Math.random() * 4);
-            gameArray.push(buttons[rndm]);
+            gameArray.push(rndm);
         }
     }
 
-    function play(interval, count) {
-
+    function play(interval, count, index) {
+        buttons[index].removeClass(inactiveColors[index]);
+        buttons[index].addClass(activeColors[index]);
+        sounds[index].play();
+        setTimeout(function () {
+            buttons[index].removeClass(activeColors[index]);
+            buttons[index].addClass(inactiveColors[index]);
+        }, 500);
+        count++;
+        if (count == turn) {
+            clearInterval(interval);
+        }
     }
-    
+
+    function blinkOff(interval, count, blinker) {
+        displayText.text("");
+        setTimeout(function () {
+            displayText.text(blinker);
+        }, 200)
+        if (count > 3) {
+            clearInterval(interval);
+        }
+    }
+
+    function blinkTurn() {
+        var blinker
+        var count = 0;
+        if (turn < 10) {
+            blinker = "0" + turn;
+        }
+        else {
+            blinker = turn;
+        }
+        displayText.text(blinker);
+        var interval = setInterval(function () {
+            count++;
+            blinkOff(interval, count, blinker);
+        }, 200)
+    }
+
     function newTurn(){
         turn++;
+        blinkTurn();
         if (turn < 10) {
             displayText.text("0" + turn);
         }
@@ -110,26 +152,32 @@ $(document).ready(function () {
         }
         var count = 0;
         var interval = setInterval(function () {
-            play(interval, count);
-        }, 1000)
+            var index = gameArray[count];
+            play(interval, count, index);
+        }, 500);
+        playerTurn = 0;
+        isPlayerTurn = true;
+        colorButton.prop("disabled", false);
     }
 
     function pressStart() {
+        clearInterval(stringInterval);
         makeArray();
-        starsCount = 1;
-        setTimeout(animationStars, 300);
         turn = 0;
         newTurn();
     }
 
     onButton.click(function () {
         if (!itsOn) {
+            onButton.prop("disabled", true);
             onAnimation();
             itsOn = true;
         }
         else {
+            onButton.prop("disabled", true);
             offAnimation();
             itsOn = false;
+            onButton.prop("disabled", false);
         }
     })
 
@@ -138,18 +186,31 @@ $(document).ready(function () {
     })
 
     colorButton.on("mousedown", function () {
-        var color = $(this).prop("id");
-        if (color == "greenButton") {
-            greenSound.play();
-        }
-        else if (color == "redButton") {
-            redSound.play();
-        }
-        else if (color == "yellowButton") {
-            yellowSound.play();
-        }
-        else if (color == "blueButton") {
-            blueSound.play();
+        if (isPlayerTurn) {
+            var color = $(this).prop("id");
+            var curr = gameArray[playerTurn];
+            if (color == buttons[curr].prop("id")) {
+                if (color == "greenButton") {
+                    greenSound.play();
+                }
+                else if (color == "redButton") {
+                    redSound.play();
+                }
+                else if (color == "yellowButton") {
+                    yellowSound.play();
+                }
+                else if (color == "blueButton") {
+                    blueSound.play();
+                }
+                if (playerTurn = turn) {
+                    isPlayerTurn = false;
+                    colorButton.prop("disabled", true);
+                    newTurn()
+                }
+                else {
+                    playerTurn++;
+                }
+            }
         }
     })
 })
