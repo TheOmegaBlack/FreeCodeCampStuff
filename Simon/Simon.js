615b3a7ea189754a348b62bddd217d6ae8ad9c35
TheOmegaBlack <sachamorgese@gmail.com>
2017-04-04 20:12:17 +0900

    Correct versions of the files

--- Simon/Simon.js
+++ Simon/Simon.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
     var startButton = $("#startButton");
     var onButton = $("#onButton");
     var strictButton = $("#strictButton");
-    var colorButton = $("color-button");
+    var colorButton = $(".color-button");
     var turn = 0;
+    var playerTurn = 0;
     var itsOn = false;
     var isPlayerTurn = false;
     var onAnim = false;
@@ -16,8 +17,11 @@ $(document).ready(function () {
     var gameArray = [];
     var starsCount;
     var scrollingString = "*******Simon freecodecamp by Sacha Morgese*******"
-    
-    
+    var greenSound = new Audio("Audio/simonSound1.mp3");
+    var redSound = new Audio("Audio/simonSound2.mp3");
+    var yellowSound = new Audio("Audio/simonSound3.mp3");
+    var blueSound = new Audio("Audio/simonSound4.mp3");
+    var sounds = [greenSound, redSound, yellowSound, blueSound];
 
     function animationStarsOff() {
         displayText.text("")
@@ -54,10 +58,13 @@ $(document).ready(function () {
         var substrSize;
         var mediaType = window.innerWidth;
         var size;
-        if (mediaType > 400){
+        if (mediaType > 400 && mediaType < 768 || mediaType > 1200) {
             size = 12;
         }
-        else{
+        else if (mediaType >= 768 && mediaType <= 800 || mediaType >= 992 && mediaType <= 1199) {
+            size = 8;
+        }
+        else {
             size = 9;
         }
         var interval = setInterval(function(){
@@ -72,13 +79,13 @@ $(document).ready(function () {
         setTimeout(animationStars, 200);
         startButton.prop("disabled", false);
         strictButton.prop("disabled", false);
-        
+        colorButton.prop("disabled", false);
+       
     }
 
     function offAnimation() {
         startButton.prop("disabled", true);
         strictButton.prop("disabled", true);
-        colorButton
     }
 
     function makeArray() {
@@ -88,9 +95,23 @@ $(document).ready(function () {
             gameArray.push(buttons[rndm]);
         }
     }
+
+    function play(interval, count) {
+
+    }
     
     function newTurn(){
-        
+        turn++;
+        if (turn < 10) {
+            displayText.text("0" + turn);
+        }
+        else {
+            displayText.text(turn);
+        }
+        var count = 0;
+        var interval = setInterval(function () {
+            play(interval, count);
+        }, 1000)
     }
 
     function pressStart() {
@@ -114,5 +135,21 @@ $(document).ready(function () {
 
     startButton.click(function () {
         pressStart();
+    })
+
+    colorButton.on("mousedown", function () {
+        var color = $(this).prop("id");
+        if (color == "greenButton") {
+            greenSound.play();
+        }
+        else if (color == "redButton") {
+            redSound.play();
+        }
+        else if (color == "yellowButton") {
+            yellowSound.play();
+        }
+        else if (color == "blueButton") {
+            blueSound.play();
+        }
     })
 })
